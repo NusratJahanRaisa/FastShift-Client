e25bf547acab121189c3956c86093c7cff70e76f
Nusrat Jahan Raisa <jahanthinks@gmail.com>
2026-03-29 18:04:58 +0600

    Landing page with hero section, FASTShift branding, and track button

--- src/Pages/LandingPage/LandingPage.jsx
+++ src/Pages/LandingPage/LandingPage.jsx
@@ -8,7 +8,42 @@ export default function LandingPage() {
       
       
       {/* Hero Section */}
-      
+      <section className=" bg-gradient-to-r from-[#f0f2f1] to-[#d9f99d] p-10 flex flex-col md:flex-row items-center justify-between min-h-screen">
+        
+        
+        {/* Left Content */}
+        <div className="max-w-xl">
+
+          <h1 className="text-[#1f2937] font-bold text-5xl leading-snug">
+          Experience <span className="text-[#84cc16] text-6xl font-bold ">FASTShift</span> – <br />
+  
+          </h1>
+          <br />
+          <h1 className="text-4xl font-bold text-[#1f2937] leading-snug">
+            We Make Sure Your <br />
+            <span className="text-[#84cc16]">Parcel Arrives</span> On Time <br />
+            – Fast, Reliable, Effortless.
+          </h1>
+
+          <p className="mt-4 text-gray-500">
+            FastShift ensures fast, reliable and hassle-free delivery tracking experience.
+          </p>
+
+          <Link to="/home">
+             <button
+                className="btn btn-lg mt-6 px-6 py-3 bg-black text-white rounded-full font-medium hover:bg-[#1f2937] transition">
+                Go and Track →
+             </button>
+          </Link>
+        </div>
+
+        {/* Right Illustration Placeholder */}
+        <div className="mt-8 md:mt-0">
+          <div className="w-[300px] h-[250px] bg-[#e5e7eb] rounded-2xl flex items-center justify-center">
+            <span className="text-gray-400">Illustration</span>
+          </div>
+        </div>
+      </section>
 
     </div>
   );
